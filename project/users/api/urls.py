e609014389cace05835471bebb6de1cd8ee1430d
max-shymanskiy <maxshymanskiy@gmail.com>
2025-04-22 22:21:57 +0300

    user-changes

--- project/users/api/urls.py
+++ project/users/api/urls.py
@@ -4,15 +4,17 @@ from .views import RegistrationView, UserViewSet, FavoriteViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 router = DefaultRouter()
-router.register(r'', UserViewSet)
+router.register(r'profile', UserViewSet)
 router.register(r'favorites', FavoriteViewSet, basename='favorites')
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls)),
 ]
